e3cb6149cff579d38354835599bc0d16424d4989
Okkeruu <kourekerouolga@gmail.com>
2026-01-07 10:55:38 +0200

    Add files via upload

--- excel_form_app/urls.py
+++ excel_form_app/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic.base import TemplateView
-
+from django.urls import path, include  # make sure to import include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path('', include('main.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),  # include your app URLs
 ]
